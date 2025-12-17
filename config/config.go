b4981c30c7b8ab32950452f5ef9b9f0cb59e63a2
linlea666 <linlea666@users.noreply.github.com>
2025-12-18 00:57:07 +0800

    config: 修改默认最大用户数从 1 改为 20

--- config/config.go
+++ config/config.go
@@ -29,7 +29,7 @@ func Init() {
 	cfg := &Config{
 		APIServerPort:       8080,
 		RegistrationEnabled: true,
-		MaxUsers:            1, // Default: only 1 user allowed
+		MaxUsers:            20, // Default: max 20 users allowed (0 = unlimited)
 	}
 
 	// Load from environment variables
